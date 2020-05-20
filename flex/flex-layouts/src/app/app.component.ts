e82be52ad8a15b8d9a9aab8762acb1951150cd70
manorath_meshram <manorathmeshram498@gmail.com>
2020-05-21 00:29:50 +0530

    blah

---
+++ flex/flex-layouts/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'flex-layouts';
+  pics= [ "/assets/w1.jpeg","/assets/eso.jpg","/assets/w1.jpeg","/assets/jumbotron_768.jpg"] 
+}
