54c7dbdd86fdb7ad106ed70561aee9a89aa068f5
manorath_meshram <manorathmeshram498@gmail.com>
2020-05-25 03:32:33 +0530

    parallax

--- flex/flex-layouts/src/app/app.component.ts
+++ flex/flex-layouts/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flex-layouts';
+  t = 0;
+  h = window.innerHeight;
   pics = [
-    {img: '/assets/w1.jpeg', name :'Roadtrips'},
-    {img: '/assets/eso.jpg', name:'Junoon'},
-    {img:'/assets/w1.jpeg', name: 'Dj war'},
-    {img:'/assets/jumbotron_1200.jpg', name:'carryminati'}
+    { img: '/assets/w1.jpeg', name: 'Roadtrips' },
+    { img: '/assets/eso.jpg', name: 'Junoon' },
+    { img: '/assets/aftermovie.jpeg', name: 'Dj war' },
+    { img: '/assets/jumbotron_1200.jpg', name: 'carryminati' },
   ];
+
+  @HostListener('window:scroll') scroll() {
+    this.t = (window.scrollY / this.h) * 100;
+  }
 }
