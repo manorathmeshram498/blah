337d5fe0b0f65f3074f272810741c4d48e582e5b
manorath_meshram <manorathmeshram498@gmail.com>
2020-08-06 17:25:30 +0530

    form added

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  lines = [];
+  submitted = false;
+  user = {
+    name: '',
+    email: '',
+    mobile_no: '',
+    insta_id: '',
+    college: '',
+    class: '',
+    profession: '',
+    date_of_birth: '',
+    language: '',
+    prev_assoc: '',
+    prev_exp: '',
+    pincode: '',
+    address: '',
+    city: '',
+    state: '',
+    gender: '',
+
+  };
+
+
+  show: boolean = false;
   title = 'form';
 }
